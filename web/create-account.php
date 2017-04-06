3ed2f52311f9d783cd85e49a9c0710c1e8168620
jkieberk <jkieberking@gmail.com>
2017-04-05 23:24:50 -0400

    initial create-account page

---
+++ web/create-account.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+
+      <div class="span3">
+      <h2>Sign Up</h2>
+      <form>
+      <label>First Name</label>
+      <input type="text" name="firstname" class="span3">
+      <label>Last Name</label>
+      <input type="text" name="lastname" class="span3">
+      <label>Email Address</label>
+      <input type="email" name="email" class="span3">
+      <label>Username</label>
+      <input type="text" name="username" class="span3">
+      <label>Password</label>
+      <input type="password" name="password" class="span3">
+      <label><input type="checkbox" name="terms"> I agree with the <a href="#">Terms and Conditions</a>.</label>
+      <input type="submit" value="Sign up" class="btn btn-primary pull-right">
+      <div class="clearfix"></div>
+      </form>
+  </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
