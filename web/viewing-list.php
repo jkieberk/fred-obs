d8fc073d2ea8264fa60194beed8c3fde69d8b8d2
jkieberk <jkieberking@gmail.com>
2017-04-08 18:25:27 -0400

    viewing-list

---
+++ web/viewing-list.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"
+      <!-- Optional theme -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+      <!-- Latest compiled and minified JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="create.css" media="screen" />
+    </head>
+  <body>
+      <div class="container">
+      <div class="row">
+          <div class="col-md-6">
+              <div class="todolist not-done">
+               <h1>Todos</h1>
+                  <input type="text" class="form-control add-todo" placeholder="Add todo">
+                      <button id="checkAll" class="btn btn-success">Mark all as done</button>
+
+                      <hr>
+                      <ul id="sortable" class="list-unstyled">
+                      <li class="ui-state-default">
+                          <div class="checkbox">
+                              <label>
+                                  <input type="checkbox" value="" />Take out the trash</label>
+                          </div>
+                      </li>
+                      <li class="ui-state-default">
+                          <div class="checkbox">
+                              <label>
+                                  <input type="checkbox" value="" />Buy bread</label>
+                          </div>
+                      </li>
+                      <li class="ui-state-default">
+                          <div class="checkbox">
+                              <label>
+                                  <input type="checkbox" value="" />Teach penguins to fly</label>
+                          </div>
+                      </li>
+                  </ul>
+                  <div class="todo-footer">
+                      <strong><span class="count-todos"></span></strong> Items Left
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-6">
+              <div class="todolist">
+               <h1>Already Done</h1>
+                  <ul id="done-items" class="list-unstyled">
+                      <li>Some item <button class="remove-item btn btn-default btn-xs pull-right"><span class="glyphicon glyphicon-remove"></span></button></li>
+
+                  </ul>
+              </div>
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
