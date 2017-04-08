2eee8794e660aba0bde6d2f406602940ecb3f45a
jkieberk <jkieberking@gmail.com>
2017-04-08 18:34:13 -0400

    viewing-5

--- web/viewing-list.php
+++ web/viewing-list.php
@@ -102,88 +102,88 @@ li.ui-state-default:last-child{
   margin-top:10px;
 }
   </style>
-</html>
+  <script>
+  $("#sortable").sortable();
+  $("#sortable").disableSelection();
 
-<script>
-$("#sortable").sortable();
-$("#sortable").disableSelection();
+  countTodos();
 
-countTodos();
+  // all done btn
+  $("#checkAll").click(function(){
+      AllDone();
+  });
 
-// all done btn
-$("#checkAll").click(function(){
-    AllDone();
-});
-
-//create todo
-$('.add-todo').on('keypress',function (e) {
-      e.preventDefault
-      if (e.which == 13) {
-           if($(this).val() != ''){
-           var todo = $(this).val();
-            createTodo(todo);
-            countTodos();
-           }else{
-               // some validation
-           }
+  //create todo
+  $('.add-todo').on('keypress',function (e) {
+        e.preventDefault
+        if (e.which == 13) {
+             if($(this).val() != ''){
+             var todo = $(this).val();
+              createTodo(todo);
+              countTodos();
+             }else{
+                 // some validation
+             }
+        }
+  });
+  // mark task as done
+  $('.todolist').on('change','#sortable li input[type="checkbox"]',function(){
+      if($(this).prop('checked')){
+          var doneItem = $(this).parent().parent().find('label').text();
+          $(this).parent().parent().parent().addClass('remove');
+          done(doneItem);
+          countTodos();
       }
-});
-// mark task as done
-$('.todolist').on('change','#sortable li input[type="checkbox"]',function(){
-    if($(this).prop('checked')){
-        var doneItem = $(this).parent().parent().find('label').text();
-        $(this).parent().parent().parent().addClass('remove');
-        done(doneItem);
-        countTodos();
-    }
-});
+  });
 
-//delete done task from "already done"
-$('.todolist').on('click','.remove-item',function(){
-    removeItem(this);
-});
+  //delete done task from "already done"
+  $('.todolist').on('click','.remove-item',function(){
+      removeItem(this);
+  });
 
-// count tasks
-function countTodos(){
-    var count = $("#sortable li").length;
-    $('.count-todos').html(count);
-}
+  // count tasks
+  function countTodos(){
+      var count = $("#sortable li").length;
+      $('.count-todos').html(count);
+  }
 
-//create task
-function createTodo(text){
-    var markup = '<li class="ui-state-default"><div class="checkbox"><label><input type="checkbox" value="" />'+ text +'</label></div></li>';
-    $('#sortable').append(markup);
-    $('.add-todo').val('');
-}
+  //create task
+  function createTodo(text){
+      var markup = '<li class="ui-state-default"><div class="checkbox"><label><input type="checkbox" value="" />'+ text +'</label></div></li>';
+      $('#sortable').append(markup);
+      $('.add-todo').val('');
+  }
 
-//mark task as done
-function done(doneItem){
-    var done = doneItem;
-    var markup = '<li>'+ done +'<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>';
-    $('#done-items').append(markup);
-    $('.remove').remove();
-}
+  //mark task as done
+  function done(doneItem){
+      var done = doneItem;
+      var markup = '<li>'+ done +'<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>';
+      $('#done-items').append(markup);
+      $('.remove').remove();
+  }
 
-//mark all tasks as done
-function AllDone(){
-    var myArray = [];
+  //mark all tasks as done
+  function AllDone(){
+      var myArray = [];
 
-    $('#sortable li').each( function() {
-         myArray.push($(this).text());
-    });
+      $('#sortable li').each( function() {
+           myArray.push($(this).text());
+      });
 
-    // add to done
-    for (i = 0; i < myArray.length; i++) {
-        $('#done-items').append('<li>' + myArray[i] + '<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>');
-    }
+      // add to done
+      for (i = 0; i < myArray.length; i++) {
+          $('#done-items').append('<li>' + myArray[i] + '<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>');
+      }
 
-    // myArray
-    $('#sortable li').remove();
-    countTodos();
-}
+      // myArray
+      $('#sortable li').remove();
+      countTodos();
+  }
 
-//remove done task from list
-function removeItem(element){
-    $(element).parent().remove();
-}
-</script>
+  //remove done task from list
+  function removeItem(element){
+      $(element).parent().remove();
+  }
+  </script>
+
+</html>
